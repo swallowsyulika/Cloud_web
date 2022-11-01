734ebf8ca8572a2939a5c84f6827e6cd895c9985
Yulika <nooseeleaf@gmail.com>
2022-11-01 10:18:20 +0800

    4.1

--- minio/main.py
+++ minio/main.py
@@ -6,8 +6,8 @@ def main():
 
     client = Minio(
         "play.min.io",
-        access_key="Q3AM3UQ867SPQQA43P2F",
-        secret_key="zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG",
+        access_key="minioadmin",
+        secret_key="minioadmin",
     )
 
     found = client.bucket_exists("minio")
